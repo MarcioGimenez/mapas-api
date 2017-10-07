6ebcc1a41a7ad00bd63fbd17f78b3a2c9054aa06
Marcio <marciogimenez1093@gmail.com>
2017-10-07 17:36:40 -0300

    comentarios

--- src/Controller/Api/V1/AplicacoesController.php
+++ src/Controller/Api/V1/AplicacoesController.php
@@ -11,11 +11,22 @@ namespace App\Controller\Api\V1;
 
 class AplicacoesController extends AppController
 {
+    #TRAIT RESPONSÁVEL PELO RESOURCE
     use \Crud\Controller\ControllerTrait;
 
+    ## MODELO PADRÃO DE METODO RESOURCE
 //    public function index(){
 //        $aplicacoes = $this->Aplicacoes->find()->all();
 //        $this->set('aplicacoes',$aplicacoes);
 //        $this->set('_serialize', ['aplicacoes']);
+//    }
+
+    #CASO QUEIRA ALTERAR O TRAIT
+//    public function view($id = null)
+//    {
+//        $this->Crud->on('beforeFind', function(\Cake\Event\Event $event) {
+//            $event->subject()->query->contain(['PropertiesTypes', 'Districts']);
+//        });
+//        return $this->Crud->execute();
 //    }
 }
